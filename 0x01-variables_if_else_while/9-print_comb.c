2d57dc344c153275fa1e168a5b887b157ac9f647
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-19 15:47:54 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,14 @@ int main(void)
 	{
 		putchar(n);
 		if (n != 57)
+
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	
 	}
-	{
-		putchar(',');
-		putchar(' ');
-	}
+
 	putchar('\n');
 	return (0);
 }
