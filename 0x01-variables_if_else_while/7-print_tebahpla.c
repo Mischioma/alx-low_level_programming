0f420be868f9c58ef772721f302daed8e2961db2
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-19 14:53:21 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase in reverse order
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for(le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar("\n");
+
+	return (0);
