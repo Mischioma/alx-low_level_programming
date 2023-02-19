be54be008b77d2f742f66e0b2adf8758518cba6c
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-19 15:09:36 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all number of base 16 in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la ,= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return(0);
+}
