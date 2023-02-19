d9e5c7aeda0cf8641d5d8f28e337b88294be7076
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-19 15:25:33 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * *main - print all possible combinations of single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0)
