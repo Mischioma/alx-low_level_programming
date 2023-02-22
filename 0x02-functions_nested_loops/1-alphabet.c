fff833ad04a691d988b9cfa8254eef7164363f84
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-22 14:46:07 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alaphabet - prints the alphabets in lowercase
+ */
+void print_alphabet(void);
+{
+	char lc;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n')
+}
