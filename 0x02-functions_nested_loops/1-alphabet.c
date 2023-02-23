dd54427758560648a292643ba0ce5bbc1988eb67
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-22 16:59:26 -0800

    int _abs(int)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,1 @@
-#include "main.h"
 
-/**
- * print_alaphabet - prints the alphabets in lowercase
- */
-void print_alphabet(void);
-{
-	char lc;
-
-	for (la = 'a'; la <= 'z'; la++)
-		_putchar(la);
-
-	_putchar('\n')
-}
