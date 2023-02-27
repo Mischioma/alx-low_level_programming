6ecd98f2419fe043884c99978067324f89c8c99a
Mischioma <chioma_ezeh52@yahoo.com>
2023-02-27 07:23:24 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	char *s = string;
+
+	putchar("The length of the string is %d", strlen(str));
+	return (string);
+}
