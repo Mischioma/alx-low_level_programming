1964a26688d3d4a4c3edb2c25fbb96f1cf3dddd7
Mischioma <chioma_ezeh52@yahoo.com>
2023-03-09 16:01:23 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int geek = 0
+		if (*s)
+		{
+			geek++;
+			geek += _strlen_recursion((s + 1);
+		}
+		return (geek);
+}
